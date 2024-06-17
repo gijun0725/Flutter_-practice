d617f41c5631437ee0539e7312b8a20fe06f3920
GiJunLee <leeslee1108@konkuk.ac.kr>
2024-06-17 11:11:25 +0900

    1

---
+++ lib/customwidget.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+class CustomWidget extends StatelessWidget {
+  const CustomWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Column(
+        children: [
+          Container(
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    width: 100,
+                    height: 200,
+                    color: Colors.red,
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    width: 100,
+                    height: 200,
+                    color: Colors.yellow,
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    width: 100,
+                    height: 200,
+                    color: Colors.orange,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 400,
+            color: Colors.lightGreen,
+            child: Row(
+              children: <Widget>[
+                Image.asset('assets/lab_instagram_icon_1.jpg'),
+                Image.asset('assets/lab_instagram_icon_2.jpg'),
+                Image.asset('assets/lab_instagram_icon_3.jpg'),
+                Spacer(),
+                Image.asset('assets/lab_instagram_icon_4.jpg')
+              ],
+            ),
+          ),
+          Container(
+            height: 200,
+            color: Colors.lightBlue,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
